07b71823db0b4711e4f94e042308d40c3dcc65ec
kimyw2357 <bubuchachakk@gmail.com>
2022-07-07 22:17:24 +0900

    react-study2

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
-  state = {
-    count: 0,
-  };
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  }
+    this.props.onIncrement(this.props.habit);
+  };
 
   handleDecrement = () => {
-    const count = this.state.count -1;
-    this.setState({ count: count < 0 ? 0 : count });
-  }
+    this.props.onDecrement(this.props.habit);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  };
 
   render() {
     const { name, count } = this.props.habit;
@@ -26,7 +26,7 @@ class Habit extends Component {
       <button className='habit-button habit-decrease' onClick={this.handleDecrement}>
         <i className="fa-solid fa-square-minus"></i>
       </button>
-      <button className='habit-button habit-delete'>
+      <button className='habit-button habit-delete' onClick={this.handleDelete}>
         <i className="fa-solid fa-trash-can"></i>        
       </button>
     </li>
